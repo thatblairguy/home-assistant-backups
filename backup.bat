c53b9a924c20e0944faab02150e71a3f5c25e96e
Blair Learn <thatblairguy@users.noreply.github.com>
2023-10-27 20:35:52 -0400

    Group directories under single timestamp.
    Add an explicit .gitignore.

--- backup.bat
+++ backup.bat
@@ -3,5 +3,6 @@ setlocal
 :: locale safe, but sufficient for my needs.
 set timestamp=%date:~10,4%-%date:~4,2%-%date:~7,2% %time:~0,2%-%time:~3,2%-%time:~6,2%
 
-robocopy \\hassio.lan\config "backup %timestamp%" /mir
-robocopy \\hassio.lan\backup "snapshots %timestamp%" /mir
+for %%a in (config backup ssl) do (
+  robocopy \\hassio.local\%%a "the-stuff\%timestamp%\%%a" /mir
+)
